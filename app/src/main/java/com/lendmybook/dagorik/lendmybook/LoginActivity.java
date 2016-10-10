a63c264215998f3c5d3ec3a85d78635867aa53e4
Dagorik <dagorik@gmail.com>
2016-10-09 23:28:24 -0500

    Obtiene le dato de la foto de perfil y el nombre

--- app/src/main/java/com/lendmybook/dagorik/lendmybook/LoginActivity.java
+++ app/src/main/java/com/lendmybook/dagorik/lendmybook/LoginActivity.java
@@ -1,17 +1,27 @@
- package com.lendmybook.dagorik.lendmybook;
+package com.lendmybook.dagorik.lendmybook;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
@@ -19,23 +29,69 @@ public class LoginActivity extends AppCompatActivity {
 
     private LoginButton loginButton;
     private CallbackManager callbackManager;
+    private ImageView imagenPerfil;
+    private String url;
+    private String nameJson;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        imagenPerfil = (ImageView) findViewById(R.id.imagen_perfil);
+
         callbackManager = CallbackManager.Factory.create();
 
         loginButton = (LoginButton) findViewById(R.id.login_button);
 
-        loginButton.setReadPermissions(Arrays.asList("email"));
+        loginButton.setReadPermissions(Arrays.asList("email,public_profile,user_photos"));
 
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
 
             @Override
             public void onSuccess(LoginResult loginResult) {
+                AccessToken accessToken = loginResult.getAccessToken();
+                Bundle params = new Bundle();
+                params.putString("fields", "id,name,email,last_name,first_name,picture");
+                GraphRequest profileGraph = GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        try {
+                            Log.i("response", "json: " + object.toString(1));
+                            JSONObject data = object.getJSONObject("picture").getJSONObject("data");
+                            url = data.getString("url");
+                            JSONObject name = object;
+                            nameJson = name.getString("name");
+                            Log.e("myLog", url);
+                            Log.e("MylogName", nameJson);
+
+
+                            SharedPreferences preferences = getSharedPreferences("sesion", getApplicationContext().MODE_PRIVATE);
+                            SharedPreferences.Editor editor = preferences.edit();
+                            editor.putString("img_perfil", url);
+                            editor.putString("name_face",nameJson);
+                            editor.commit();
+
+
+//
+//                            urlPerfil = preferences.getString("img_perfil",null);
+//
+//                            Log.e("myLogPerfil", urlPerfil);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+
+                        }
+                    }
+
+                });
+                profileGraph.setParameters(params);
+                profileGraph.executeAsync();
                 goMainScreen();
+
+
             }
 
             @Override
--- app/src/main/java/com/lendmybook/dagorik/lendmybook/MainActivity.java
+++ app/src/main/java/com/lendmybook/dagorik/lendmybook/MainActivity.java
@@ -1,27 +1,66 @@
 package com.lendmybook.dagorik.lendmybook;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginManager;
+import com.lendmybook.dagorik.lendmybook.fragmentes.OneFragment;
+import com.lendmybook.dagorik.lendmybook.fragmentes.ThreeFragment;
+import com.lendmybook.dagorik.lendmybook.fragmentes.TwoFragment;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private Button logOut;
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private String urlPerfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        logOut = (Button) findViewById(R.id.btn_logOut);
+        toolbar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.iablayout);
+
+        tabLayout.setupWithViewPager(viewPager);
+
 
         if (AccessToken.getCurrentAccessToken() == null) {
             goLoginScreen();
+
+            SharedPreferences preferences = getSharedPreferences("sesion", getApplicationContext().MODE_PRIVATE);
+            urlPerfil = preferences.getString("img_perfil",null);
+
+            Log.e("myLogPerfil", urlPerfil);
+
+
         }
 
     }
@@ -37,4 +76,44 @@ public class MainActivity extends AppCompatActivity {
         goLoginScreen();
 
     }
+
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new OneFragment(), "PERFIL");
+        adapter.addFragment(new TwoFragment(), "CATALOGO");
+        adapter.addFragment(new ThreeFragment(), "NOTIFICACIONES");
+        viewPager.setAdapter(adapter);
+    }
+
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
 }
